532defccd2d55eb47f70a4e81fd424bcd97fb441
Fernando Unger <87918106+fernandounger@users.noreply.github.com>
2022-11-11 19:58:33 -0300

    feat: route default

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { DrawGameComponent } from './components/draw-game/draw-game.component';
 import { DrawResultComponent } from './components/draw-result/draw-result.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full' },
   {path: 'home', component: DrawGameComponent },
   {path: 'result', component: DrawResultComponent}
 ];
